8dccecc3161ba0ec45e3b19adef03699d4a5e979
jmfolcik <60854030+jmfolcik@users.noreply.github.com>
2020-02-09 12:26:57 -0600

    version 1.0

---
+++ vncautoban.ps1
@@ -0,0 +1,40 @@
+function addtoautoban
+{
+param ($addthisone)
+$remoteaddresses = @()
+$remoteaddresses += (Get-NetFirewallRule -DisplayName 'vnc autoban' | Get-NetFirewallAddressFilter).RemoteAddress
+if ($remoteaddresses -notcontains $addthisone)
+    {
+        $remoteaddresses += $addthisone
+        Set-NetFirewallRule -DisplayName 'vnc autoban' -RemoteAddress $remoteaddresses
+    }
+}
+
+$time = (Get-Date).AddMinutes(-10)
+$filterhashtable = @{
+    LogName = 'Application'
+    ProviderName = 'tvnserver'
+    StartTime = $time
+}
+
+$events = Get-WinEvent -FilterHashtable $filterhashtable -MaxEvents 40
+$ips = @()
+$regex = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+foreach ($event in $events)
+    {
+        if ($event.message -like 'Authentication failed*')
+            {
+            $event.message -match $regex
+            $ips += $matches.0
+            }
+    }
+
+$frequency = $ips | group
+
+foreach ($freak in $frequency)
+    {
+    if ($freak.count -ge 3)
+        {
+        addtoautoban $freak.name
+        }
+    }
