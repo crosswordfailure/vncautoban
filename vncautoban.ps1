d33ab14d18dbcacd3958aeb4ecbeafbb8a350aa1
crosswordfailure <60854030+crosswordfailure@users.noreply.github.com>
2020-02-15 15:50:32 -0600

    Update vncautoban.ps1

--- vncautoban.ps1
+++ vncautoban.ps1
@@ -1,6 +1,24 @@
+function firstfirewallrule
+{
+    New-NetFirewallRule -DisplayName 'vnc autoban' -Profile Any -Direction Inbound -Action Block -Enabled False
+    Get-NetFirewallRule -DisplayName 'vnc autoban' | Get-NetFirewallPortFilter | Set-NetFirewallPortFilter -Protocol TCP -LocalPort 5900
+}
+
+function rollfirewallrule
+{
+    Get-NetFirewallRule -DisplayName 'vnc autoban' | Set-NetFirewallRule -NewDisplayName ('vnc autoban ' + (get-date -Format 'MM-dd-yyyy--HH-mm'))
+    New-NetFirewallRule -DisplayName 'vnc autoban' -Profile Any -Direction Inbound -Action Block -Enabled False
+    Get-NetFirewallRule -DisplayName 'vnc autoban' | Get-NetFirewallPortFilter | Set-NetFirewallPortFilter -Protocol TCP -LocalPort 5900
+}
+
 function addtoautoban
 {
 param ($addthisone)
+$rulesize = ((Get-NetFirewallRule -DisplayName 'vnc autoban' | Get-NetFirewallAddressFilter).RemoteAddress).count
+if ($rulesize -ge 512)
+ {
+     rollfirewallrule
+ }
 $remoteaddresses = @()
 $remoteaddresses += (Get-NetFirewallRule -DisplayName 'vnc autoban' | Get-NetFirewallAddressFilter).RemoteAddress
 if ($remoteaddresses -notcontains $addthisone)
@@ -8,7 +26,17 @@ if ($remoteaddresses -notcontains $addthisone)
         $remoteaddresses += $addthisone
         Set-NetFirewallRule -DisplayName 'vnc autoban' -RemoteAddress $remoteaddresses
     }
+if ((Get-NetFirewallRule -DisplayName 'vnc autoban').enabled -eq 'False')
+    {
+        Set-NetFirewallRule -DisplayName 'vnc autoban' -Enabled True
+    }
 }
+
+$rule = Get-NetFirewallRule -DisplayName 'vnc autoban'
+if ($rule -eq $null)
+    {
+    firstfirewallrule
+    }
 
 $time = (Get-Date).AddMinutes(-10)
 $filterhashtable = @{
@@ -33,7 +61,7 @@ $frequency = $ips | group
 
 foreach ($freak in $frequency)
     {
-    if ($freak.count -ge 3)
+    if ($freak.count -ge 2)
         {
         addtoautoban $freak.name
         }
